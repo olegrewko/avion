637777cbbf5866d6ff4969c2a35483c53ff8e440
olegrewko <olegrewko@gmail.com>
2024-05-27 12:02:26 +0300

    end

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const noticeEl = document.querySelector('.notice');
-const  = document.querySelector(".stepper");
+const stepperEl = document.querySelector(".stepper");
 
 if(noticeEl) {
   const noticeCloseEl = noticeEl.querySelector(".notice__close");
@@ -14,4 +14,12 @@ if(stepperEl) {
   const stepperBtnPlusEl = stepperEl.querySelector('.stepper__btn--plus');
 
   let count = Number(stepperInputEl.value);
+  stepperBtnPlusEl.addEventListener('click', () => {
+    stepperBtnPlusEl.classList.remove('stepper__btn--disabled');
+    count++;
+    stepperInputEl.value = count;
+  }
+  if(stepper === stepperMax) {
+    stepperBtnPlusEl.classList.add('stepper__btn--disabled')
+  });
 }
